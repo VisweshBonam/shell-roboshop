e501ca5f52999722ee60b0e79ea895d2ea88e88f
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-20 10:37:59 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -17,4 +17,22 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-
     fi
     echo "$instance IP address: $IP"
 
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+
 done
