03677ae0eefda366dc0ce5420616f7b8161a76b7
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-20 09:45:56 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+AMI-ID="ami-09c813fb71547fc4f"
+Security_GroupId="sg-07bf1ea3d5397ec68"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+Zone_ID="Z01891953PLU1K6VRGBFE"
+Domain_Name="liveyourlife.site"
+
+for instances in ${INSTANCES[@]}
+do
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07bf1ea3d5397ec68 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address: $IP"
+
+done
