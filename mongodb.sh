b88d9d2a43a3bca20dec899d2423cd5313640302
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-21 06:20:26 +0530

    Configuring mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -29,10 +29,11 @@ VALIDATE() {
         echo -e "$2 is ....$G  SUCCESS ...$N" | tee -a $LOG_FILE
     else
         echo -e "$2 is.... $R  FAILURE...$N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongodb.repo
+cp mongo.repo /etc/yum.repos.d/mongodb.repo 
 VALIDATE $? "Copying Mongodb"
 
 dnf install mongodb-org -y &>>$LOG_FILE
