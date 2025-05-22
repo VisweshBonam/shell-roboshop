fb0c6ad8de1f0048454916d741ce4f354bdd1839
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-22 14:11:26 +0530

    all

--- user.sh
+++ user.sh
@@ -13,7 +13,7 @@ N="\e[0m"
 #Logs
 
 LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME="$(echo $0 | awk -F "." '$print{1F}')" 
+SCRIPT_NAME="$(echo $0 | awk -F "." 'print{$1F}')" 
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FILE
