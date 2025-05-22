2e7a1b9549b37f37fc0b59f598cdbacf7e54313f
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-22 14:06:09 +0530

    all

--- user.sh
+++ user.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
+START_TIME="$(date +%s)"
 
-UserId=$(id -u)
+UserId="$(id -u)"
 
 #colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#Logs
+
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME="$(echo $0 | awk -F "." 'print{1F}')" 
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FILE
+
+echo "script name : $SCRIPT_NAME"
