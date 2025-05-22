1fd8c309ba180fd5543dbcf2c72710022984479f
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-22 16:58:20 +0530

    all

--- cart.sh
+++ cart.sh
@@ -4,35 +4,27 @@ START_TIME="$(date +%s)"
 
 UserId="$(id -u)"
 
-#colors
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
 #Logs
 
 LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME="$(echo $0 | awk -F "." '{print $1F}')" 
+SCRIPT_NAME="$(echo $0 | awk -F "." '{print $1F}')"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR="$PWD"
 
 mkdir -p $LOG_FOLDER
 
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+echo -e "Script started executing at : $Y $(date) $N"
 
-if [ $UserId != 0 ]
-then
+if [ $UserId != 0 ]; then
     echo -e "$R ERROR $N :: Please access with root access" | tee -a $LOG_FILE
     exit 1
 else
     echo -e "$G You are the root user $N" | tee -a $LOG_FILE
 fi
 
-VALIDATE(){
-    if [ $1 == 0 ]
-    then
-        echo -e "$2 is ......$G Success $N" | tee -a $LOG_FILE 
+VALIDATE() {
+    if [ $1 == 0 ]; then
+        echo -e "$2 is ......$G Success $N" | tee -a $LOG_FILE
     else
         echo -e "$2 is ......$R Failed $N" | tee -a $LOG_FILE
         exit 1
@@ -40,51 +32,48 @@ VALIDATE(){
 }
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling Nodejs"
+VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Nodejs"
+VALIDATE $? "Enabling nodejs"
 
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing Nodejs"
+VALIDATE $? "Installing nodejs"
 
-mkdir -p /app 
+mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app directory"
 
 id roboshop
-if [ $? != 0 ]
-then
+if [ $? != 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating System User"
+    VALIDATE $? "Roboshop user created"
 else
-    echo -e "System User already exist"
+    echo -e "Roboshop system user already exsists : $Y SKIPPING....$N"
 fi
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE 
-VALIDATE $? "Downloading User Code"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading cart file"
 
 rm -rf /app/*
 cd /app
-
-unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping user file"
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping cart file"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing packages"
+VALIDATE $? "Installing Dependencies"
 
-
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
-VALIDATE $? "copying user service"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$LOG_FILE
+VALIDATE $? "Copying cart service"
 
 systemctl daemon-reload &>>$LOG_FILE
 
-systemctl enable user &>>$LOG_FILE
-VALIDATE $? "Enabling User"
+systemctl enable cart &>>$LOG_FILE
+VALIDATE $? "Enabling Cart"
 
-systemctl start user
+systemctl start cart &>>$LOG_FILE
 VALIDATE $? "Starting User"
 
 END_TIME="$(date +%s)"
-
 TOTAL_TIME="$(($END_TIME - $START_TIME))"
+
 echo -e "Script execution is completed..Time taken to execute the script is : $Y $TOTAL_TIME $N"
