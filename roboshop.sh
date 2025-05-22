6c5c8951b89430257e11bd39aca079d161cfd4d2
Visweshbonam <viswesh.bonam@apoorva.com>
2025-05-22 12:50:16 +0530

    all

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ Zone_ID="Z01891953PLU1K6VRGBFE"
 Domain_Name="liveyourlife.site"
 
 #for instance in ${INSTANCES[@]}
-for insatnce in $@; do
+for instance in $@; do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07bf1ea3d5397ec68 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
